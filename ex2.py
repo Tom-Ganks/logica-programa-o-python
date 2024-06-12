27b3622a2f9ef1b10fb45d2dca09dffe41efcb47
naofumi <kingnaofumi007@gmail.com>
2024-06-11 21:39:22 -0300

    exercicio 2 completo incluido

---
+++ ex2.py
@@ -0,0 +1,10 @@
+altura = float(input('digite sua altura: '))
+sexo = input('digite seu sexo: ')
+
+if sexo == "Masculino":
+    pesoIdeal = (72.7 * altura) - 58
+elif sexo == "Feminino":
+    pesoIdeal = (62.1 * altura) - 44.7
+else:
+    pesoIdeal = ("Attack Helicopter")
+print('Seu peso ideal é', sexo, pesoIdeal, "quilos")
