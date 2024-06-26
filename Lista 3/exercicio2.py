687c9ceaa89c2fa50895494a022d559d486308e0
naofumi <kingnaofumi007@gmail.com>
2024-06-25 21:52:54 -0300

    correção

--- Lista 3/exercicio2.py
+++ Lista 3/exercicio2.py
@@ -1,18 +1,16 @@
 Q = []
 P = []
+total = 0
+vendas = int(input("Digite a quantidade de venda: "))
 
-for i in range(10):
-    Quantidade = int(input("Digite a quantidade de mercadorias vendidas{i}: "))
+for i in range(vendas):
+    quant = int(input("Digite a quantidade de mercadorias vendidas{i}: "))
     preco = float(input("Digite o preço das mercadorias vendidas{i}: "))
-    Q.append(Quantidade)
+    Q.append(quant)
     P.append(preco)
+    total += quant * preco
 
-    if len(Q) != len(P):
-        print("Erro: As listas Q e P devem ter o mesmo comprimento.")
+print(f"Lista de quantidades: {Q:.2f}")
+print(f"lista dos precos: {P:.2f}")
 
-
-    faturamento = 0.0
-    for i in range(10):
-        faturamento = faturamento + (Q[i] * P[i])
-
-        print(f"O faturamento total é R$: {faturamento}")
+print(f"O faturamento mensal é de: {total}")
